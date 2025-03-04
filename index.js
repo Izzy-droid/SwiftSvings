ad399849ca46f65e98d52588b739219045e77ddf
Izzy-droid <isdm908@gmail.com>
2025-03-04 14:41:57 -0500

    Added html, css, picture, aand other relevant files

---
+++ index.js
@@ -0,0 +1,15 @@
+const searchBox = document.getElementsByClassName('.search-box');
+//takes the search box
+
+//p.innerText = goes into the content of the tag
+//p.innerHTML = same as innerText but includes tags
+//p.innerText, assigning new value changes it
+//getElementsByClassName, brings a collections of tags with the same class. allows class changes en masse if done right
+//getElementsByTagName= same as above but with tags like <p> or <div>
+//document.querySelector, grabs the first element that is put
+//document.querySelectorAll, grabs all elements that are put
+
+//when the search text box is getting typed into, it searches for items
+
+let row_1= document.createElement('')
+const searchStores = document.getElementsByTagName('body')[0];
